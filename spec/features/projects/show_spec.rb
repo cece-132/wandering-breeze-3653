bb8d7fff38be50f38866168f86f9405672fc37df
Candace Eckels <candace.eckels@gmail.com>
2022-09-06 15:38:47 -0600

    Written feature/model test for #contestant_count

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -15,6 +15,28 @@ RSpec.describe Project do
       end
     end
   end
+
+  describe 'User Story 3' do
+    it 'counts the numeber of contestants on a project' do
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+
+      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+      kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+
+      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: kentaro.id, project_id: news_chic.id)
+
+      visit project_path(news_chic)
+
+      within "#project-#{news_chic.id}" do
+        expect(page).to have_content("Number of Contestants: 3")
+      end
+    end
+  end
 end
 
 # recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
