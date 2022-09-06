df0c5732bd693a6a3d51de862af2f0cee7b30eae
Candace Eckels <candace.eckels@gmail.com>
2022-09-06 16:24:23 -0600

    model test for contestant ids

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -59,7 +59,29 @@ RSpec.describe Project do
       end
     end
   end
-  
+
+  describe 'EXTENSION 2' do
+    it 'can add a contestant to a project' do
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+
+      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+      kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+
+      jay_nc = ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+      gretchen_nc = ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+  #     ContestantProject.create(contestant_id: kentaro.id, project_id: news_chic.id)
+
+      visit project_path(news_chic)
+
+      within ".add_contestant" do
+        fill_in
+      end
+    end
+  end
+
 end
 
 # recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -50,4 +50,24 @@ RSpec.describe Project, type: :model do
       end
     end
   end
+
+  describe "Class Methods" do
+    
+    describe ".names" do
+      it "pulls a list of all the contestant names" do
+        recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+
+        news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+  
+        jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+        gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+        kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+  
+        jay_nc = ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+        gretchen_nc = ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+
+        expect(Contestant.ids).to eq [jay.id, gretchen.id, kentaro.id]
+      end
+    end
+  end
 end
